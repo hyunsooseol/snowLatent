4291e7a24617835960b5efe7f76967767a839d9e
hyunsooseol <snow@cau.ac.kr>
2022-08-18 15:24:12 +0900

    First commit

---
+++ R/lca.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+lcaClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "lcaClass",
+    inherit = lcaBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
