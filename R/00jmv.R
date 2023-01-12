e0c5572020dbae11eec4a3e63df7158db058cc90
hyunsooseol <snow@cau.ac.kr>
2023-01-12 09:20:17 +0900

    Updates 2.2.9

--- R/00jmv.R
+++ R/00jmv.R
@@ -7,7 +7,7 @@
         `author`="Seol, H.", 
         `year`=2023, 
         `title`="snowLatent: Latent Class Analysis for jamovi", 
-        `publisher`="(Version 2.2.8)[jamovi module]. URL https://github.com/hyunsooseol/snowLatent", 
+        `publisher`="(Version 2.2.9)[jamovi module]. URL https://github.com/hyunsooseol/snowLatent", 
         `url`="https://github.com/hyunsooseol/snowLatent"), 
     `glca`=list(
         `type`="software", 
