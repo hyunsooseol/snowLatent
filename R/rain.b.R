bbcbf149ecc3e79dbc003d883b36d91933d85304
hyunsooseol <snowcau@gmail.com>
2024-07-29 15:12:48 +0900

    delete PupillometryR package

--- R/rain.b.R
+++ R/rain.b.R
@@ -3,7 +3,7 @@
 #' @importFrom R6 R6Class
 #' @import jmvcore
 #' @import ggplot2
-#' @import PupillometryR geom_flat_violin
+#' @import dplyr
 #' @importFrom ggplot2 ggplot
 #' @importFrom reshape2 melt
 #' @export
@@ -84,8 +84,104 @@ rainClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
         
         plotData <- image$state
         
+        #####################################
+        library(ggplot2)
+        library(dplyr)
+        
+        
+        "%||%" <- function(a, b) {
+          if (!is.null(a))
+            a
+          else
+            b
+        }
+        
+        GeomFlatViolin <-
+          ggplot2::ggproto(
+            "GeomFlatViolin",
+            ggplot2::Geom,
+            setup_data = function(data, params) {
+              data$width <- data$width %||%
+                params$width %||% (ggplot2::resolution(data$x, FALSE) * 0.9)
+              
+              # ymin, ymax, xmin, and xmax define the bounding rectangle for each group
+              data %>%
+                dplyr::group_by(.data = ., group) %>%
+                dplyr::mutate(
+                  .data = .,
+                  ymin = min(y),
+                  ymax = max(y),
+                  xmin = x,
+                  xmax = x + width / 2
+                )
+            },
+            
+            draw_group = function(data, panel_scales, coord)
+            {
+              # Find the points for the line to go all the way around
+              data <- base::transform(data,
+                                      xminv = x,
+                                      xmaxv = x + violinwidth * (xmax - x))
+              
+              # Make sure it's sorted properly to draw the outline
+              newdata <-
+                base::rbind(
+                  dplyr::arrange(.data = base::transform(data, x = xminv), y),
+                  dplyr::arrange(.data = base::transform(data, x = xmaxv), -y)
+                )
+              
+              # Close the polygon: set first and last point the same
+              # Needed for coord_polar and such
+              newdata <- rbind(newdata, newdata[1,])
+              
+              ggplot2:::ggname("geom_flat_violin",
+                               GeomPolygon$draw_panel(newdata, panel_scales, coord))
+            },
+            
+            draw_key = ggplot2::draw_key_polygon,
+            
+            default_aes = ggplot2::aes(
+              weight = 1,
+              colour = "grey20",
+              fill = "white",
+              size = 0.5,
+              alpha = NA,
+              linetype = "solid"
+            ),
+            
+            required_aes = c("x", "y")
+          )
+        
+        
+        geom_flat_violin <-
+          function(mapping = NULL,
+                   data = NULL,
+                   stat = "ydensity",
+                   position = "dodge",
+                   trim = TRUE,
+                   scale = "area",
+                   show.legend = NA,
+                   inherit.aes = TRUE,
+                   ...) {
+            ggplot2::layer(
+              data = data,
+              mapping = mapping,
+              stat = stat,
+              geom = GeomFlatViolin,
+              position = position,
+              show.legend = show.legend,
+              inherit.aes = inherit.aes,
+              params = list(trim = trim,
+                            scale = scale,
+                            ...)
+            )
+          }
+        
+        ##################################
+        
         plot <- ggplot2::ggplot(plotData, ggplot2::aes(x = Variable, y = Value, fill = Group)) +
-          PupillometryR::geom_flat_violin(ggplot2::aes(fill = Group), 
+          
+                geom_flat_violin(ggplot2::aes(fill = Group), 
                                           position = ggplot2::position_nudge(x = .2, y = 0), 
                                           adjust = 1.5, trim = FALSE, alpha = .5, colour = NA,
                                           width = 1.0, scale = "width") +
